0ad6066ff7910a678b8ae1e0abe8e8db1eb798c5
Mars <719911972@qq.com>
2020-03-05 22:48:13 +0800

    22:48 3.5 Mars

--- frontend/src/Containers/flow/index.tsx
+++ frontend/src/Containers/flow/index.tsx
@@ -1,10 +1,27 @@
 import React from "react"
+import {Button} from "antd";
+import flow_style from "./index.module.scss"
+import image from "../../Assets/image_demo.jpg"
+import mask from "../../Assets/mask_demo.jpg"
 
-const PageFlow=() => {
+const PageFlow = () => {
     return (
         <div>
+            <div className={flow_style.upload}>
+                <Button type="primary" size={"large"}>
+                    上传图片
+                </Button>
+            </div>
+            <div className={flow_style.image}>
+                <img src={image} alt={"示例图片"}/>
+
+            </div>
+            <div className={flow_style.image}>
+                <img src={mask} alt={"mask"}/>
+
+            </div>
             <span>
-                这里是主页
+                Mask
             </span>
         </div>
     )
